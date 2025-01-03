ee22b62cc9396bb0160243da64ce04a3b176ba77
tuanbin298 <tuannase171419@fpt.edu.vn>
2025-01-03 19:32:47 +0700

    Update

--- koi_be/keystone.ts
+++ koi_be/keystone.ts
@@ -26,8 +26,9 @@ export default withAuth(
       // we're using sqlite for the fastest startup experience
       //   for more information on what database might be appropriate for you
       //   see https://keystonejs.com/docs/guides/choosing-a-database#title
-      provider: "postgresql",
-      url: process.env.POSTGRE_URL || "file:./keystone.db",
+      provider: "sqlite",
+      // url: process.env.POSTGRE_URL || "file:./keystone.db",
+      url: "file:./keystone.db",
     },
     lists,
     graphql: {
